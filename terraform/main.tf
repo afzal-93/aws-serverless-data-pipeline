c705f5661f89342c1c585fe487986a5f85c8ad5c
afzal-93 <afzalahmed93@hotmail.co.uk>
2025-08-06 15:09:09 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,10 @@
 resource "aws_s3_bucket" "raw_data" {
   bucket = var.s3_raw_bucket_name
+  resource "aws_s3_bucket_acl" "raw_data_acl" {
+  bucket = aws_s3_bucket.raw_data.id
   acl    = "private"
+}
+
 }
 
 resource "aws_db_instance" "postgres" {
@@ -8,9 +12,10 @@ resource "aws_db_instance" "postgres" {
   engine              = "postgres"
   instance_class      = "db.t3.micro"
   allocated_storage   = 20
-  name                = var.rds_db_name
+  db_name = var.rds_db_name
   username            = var.rds_username
   password            = var.rds_password
   publicly_accessible = true
   skip_final_snapshot = true
 }
+
