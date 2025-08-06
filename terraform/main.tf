8b3b38abfb0aae55950ec1f4483ce3cfc9e86582
afzal-93 <afzalahmed93@hotmail.co.uk>
2025-08-06 15:14:32 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,23 @@
+# Create the S3 bucket
 resource "aws_s3_bucket" "raw_data" {
-  bucket = var.s3_raw_bucket_name
-  resource "aws_s3_bucket_acl" "raw_data_acl" {
+  bucket = var.raw_bucket_name
+}
+
+# Set ACL for the S3 bucket
+resource "aws_s3_bucket_acl" "raw_data_acl" {
   bucket = aws_s3_bucket.raw_data.id
   acl    = "private"
 }
 
-}
-
+# Create RDS instance
 resource "aws_db_instance" "postgres" {
-  identifier          = "processed-data-db"
-  engine              = "postgres"
-  instance_class      = "db.t3.micro"
-  allocated_storage   = 20
-  db_name = var.rds_db_name
-  username            = var.rds_username
-  password            = var.rds_password
-  publicly_accessible = true
-  skip_final_snapshot = true
+  identifier             = var.rds_db_identifier
+  allocated_storage      = 20
+  engine                 = "postgres"
+  engine_version         = "13.4"
+  instance_class         = "db.t3.micro"
+  username               = var.db_username
+  password               = var.db_password
+  skip_final_snapshot    = true
+  publicly_accessible    = true
 }
-
